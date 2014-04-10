ba97b7384aca2b6903aa922947a4a17f4e729def
Stéphane Klein <contact@stephane-klein.info>
2014-04-10 11:45:15 +0200

    Upgrade with new method

--- bootstrap.py
+++ bootstrap.py
@@ -1,17 +1,60 @@
-import os
-import sys
+#!/usr/bin/env python
+## -*- coding: utf-8 -*-
+"""Create a custom "virtual" Python installation
+"""
+
+import virtualenv
+
+from os.path import join as path_join
+from tempfile import mkdtemp
+from subprocess import call as subprocess_call
+import textwrap
+from shutil import rmtree
 
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen  # NOQA
 
-print("""
-This bootstrap install virtualenv instance in your working folder (%s).
-Warning : this file isn't buildout bootstrap.py
-""" % os.getcwd())
+tmp_dir = None
 
-sys.argv = ['bootstrap.py', '.']
-exec(
-    urlopen('https://raw.github.com/pypa/virtualenv/develop/virtualenv.py').read()
-)
+
+def run(cmd):
+    print(cmd)
+    subprocess_call(cmd, shell=True)
+
+
+def local_adjust_options(options, args):
+    global tmp_dir
+
+    tmp_dir = mkdtemp()
+    print('download setuptools...')
+    f = open(path_join(tmp_dir, 'setuptools-latest.tar.gz'), 'w')
+    f.write(urlopen('https://pypi.python.org/packages/source/s/setuptools/setuptools-2.0.2.tar.gz').read())  # NOQA
+    f.close()
+    print('setuptools downloaded')
+
+    print('download pip...')
+    f = open(path_join(tmp_dir, 'pip-latest.tar.gz'), 'w')
+    f.write(urlopen('https://pypi.python.org/packages/source/p/pip/pip-1.5.tar.gz').read())  # NOQA
+    f.close()
+    print('pip downloaded')
+
+    if len(args) == 0:
+        options.search_dirs = [tmp_dir]
+        args.append('.')
+
+
+def local_after_install(options, home_dir):
+    global tmp_dir
+
+    run('./bin/pip install -r requirements.txt')
+
+    rmtree(tmp_dir)
+
+# Monkey patch
+virtualenv.adjust_options = local_adjust_options
+virtualenv.after_install = local_after_install
+
+
+virtualenv.main()
